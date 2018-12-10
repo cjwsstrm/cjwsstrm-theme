497008ffae4ec3e9ba854a0edc652132705166cd
CJ <cj@gokonstruct.com>
2018-12-09 23:22:13 -0700

    updated styling and layout

--- src/header.php
+++ src/header.php
@@ -45,13 +45,13 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'cjwsstrm' ); ?></button>
-			<!-- <?php
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Main Menu', 'cjwsstrm' ); ?></button>
+			<?php
 			wp_nav_menu( array(
 				'theme_location' => 'menu-1',
 				'menu_id'        => 'primary-menu',
 			) );
-			?> -->
+			?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
