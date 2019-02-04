1f7461007b3dc11a3b38a6b6fbd39ac1e89ae622
cjwsstrm <cj@gokonstruct.com>
2019-02-03 21:53:02 -0700

    added script to loop through menu items and rearrange them

--- src/js/cjwsstrm-public.js
+++ src/js/cjwsstrm-public.js
@@ -1,8 +1,67 @@
-const dataTestTesting = [1,2,3,4,5,6];
 
-let testBabel1 = dataTestTesting.map((n) => n * 3);
+// Set current menu item to center position in list
+window.addEventListener('load', function() {
+  console.log('All assets are loaded')
+  let mainMenu = document.querySelector('#primary-menu');
+  // console.log(mainMenu);
+  let menuItems = mainMenu.querySelectorAll('li');
+  // console.log(menuItems);
+  for (var i = 0; i < menuItems.length; i++) {
+    menuItems[i].addEventListener('click', printDetails);
+  }
+  
+  function printDetails(e) {
+    e.preventDefault();
+    console.log(e);
+    console.log(this);
+  }
 
-const data2 = ['1','2','3','4','5'];
+  const centerIndex = (arr, number) => {
+    let targetIndex = arr.indexOf(number);
+    if (targetIndex < 0) {
+      console.log(`The number you provided is not part of the array.`);
+      return;
+    }
+    let newPosition = 0;
+    if (arr.length % 2 != 0) {
+      newPosition = Math.floor(arr.length / 2);
+    } else {
+      newPosition = arr.length / 2;
+    }
+    console.log(targetIndex);
+    console.log(arr[newPosition]);
+    console.log(arr.indexOf(arr[newPosition]));
+    console.log(arr.length)
+    console.log(`arr before: ${arr}`);
+    let steps = Math.abs(targetIndex - arr.indexOf(arr[newPosition]));
+    let appendLast = [];
+    if (targetIndex > newPosition) {
+    for (i = 0; i < arr.length; i++) {
+      if ((i - steps) < 0 ) {
+        console.log( i - steps);
+        appendLast.push(arr[i]);
+      }
+    }
+    for (i = 0; i < appendLast.length; i++) {
+      arr.splice(0, 1); // Remove the first items in the array
+      arr.push(appendLast[i]); // Add them to the back of the array
+      }
+    }
+    if (targetIndex < newPosition) {
+      for (i = 0; i < arr.length; i++) {
+        if ((i + steps) > arr.length - 1 ) {
+          // console.log(appendLast)
+          appendLast.push(arr[i]);
+        } 
+      }
+      for (i = 0; i < appendLast.length; i++) {
+        arr.splice((arr.length - 1), 1); // Remove the last items in the array
+        arr.unshift(appendLast[i]); // Add them to the front of the array
+      }
+    }
+    console.log(`steps: ${steps}`);
+    console.log(`appendLast: ${appendLast}`);
+    console.log(`arr after: ${arr}`);
+  }
 
-let testBabel22 = data2.filter((n) => parseInt(n) * 10);
-
+})
