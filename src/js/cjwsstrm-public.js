c23c81cd723bfeed95fcecb730f01b24220b6f84
CJ <cj@gokonstruct.com>
2020-05-02 10:08:11 -0600

    disabled menu item centering and updated page specific styling

--- src/js/cjwsstrm-public.js
+++ src/js/cjwsstrm-public.js
@@ -61,53 +61,51 @@ window.addEventListener('load', function() {
     console.log(`steps: ${steps}`);
     console.log(`arr after: ${mainMenuArray}`);
   };
-
-  const centerIndex = (arr, number) => {
-    let targetIndex = arr.indexOf(number);
-    if (targetIndex < 0) {
-      console.log(`The number you provided is not part of the array.`);
-      return;
-    }
-    let newPosition = 0;
-    if (arr.length % 2 != 0) {
-      newPosition = Math.floor(arr.length / 2);
-    } else {
-      newPosition = arr.length / 2;
-    }
-    console.log(targetIndex);
-    console.log(arr[newPosition]);
-    console.log(arr.indexOf(arr[newPosition]));
-    console.log(arr.length)
-    console.log(`arr before: ${arr}`);
-    let steps = Math.abs(targetIndex - arr.indexOf(arr[newPosition]));
-    let appendLast = [];
-    if (targetIndex > newPosition) {
-    for (i = 0; i < arr.length; i++) {
-      if ((i - steps) < 0 ) {
-        console.log( i - steps);
-        appendLast.push(arr[i]);
-      }
-    }
-    for (i = 0; i < appendLast.length; i++) {
-      arr.splice(0, 1); // Remove the first items in the array
-      arr.push(appendLast[i]); // Add them to the back of the array
-      }
-    }
-    if (targetIndex < newPosition) {
-      for (i = 0; i < arr.length; i++) {
-        if ((i + steps) > arr.length - 1 ) {
-          // console.log(appendLast)
-          appendLast.push(arr[i]);
-        } 
-      }
-      for (i = 0; i < appendLast.length; i++) {
-        arr.splice((arr.length - 1), 1); // Remove the last items in the array
-        arr.unshift(appendLast[i]); // Add them to the front of the array
-      }
-    }
-    console.log(`steps: ${steps}`);
-    console.log(`appendLast: ${appendLast}`);
-    console.log(`arr after: ${arr}`);
-  }
-
 })
+  // const centerIndex = (arr, number) => {
+  //   let targetIndex = arr.indexOf(number);
+  //   if (targetIndex < 0) {
+  //     console.log(`The number you provided is not part of the array.`);
+  //     return;
+  //   }
+  //   let newPosition = 0;
+  //   if (arr.length % 2 != 0) {
+  //     newPosition = Math.floor(arr.length / 2);
+  //   } else {
+  //     newPosition = arr.length / 2;
+  //   }
+  //   console.log(targetIndex);
+  //   console.log(arr[newPosition]);
+  //   console.log(arr.indexOf(arr[newPosition]));
+  //   console.log(arr.length)
+  //   console.log(`arr before: ${arr}`);
+  //   let steps = Math.abs(targetIndex - arr.indexOf(arr[newPosition]));
+  //   let appendLast = [];
+  //   if (targetIndex > newPosition) {
+  //   for (i = 0; i < arr.length; i++) {
+  //     if ((i - steps) < 0 ) {
+  //       console.log( i - steps);
+  //       appendLast.push(arr[i]);
+  //     }
+  //   }
+  //   for (i = 0; i < appendLast.length; i++) {
+  //     arr.splice(0, 1); // Remove the first items in the array
+  //     arr.push(appendLast[i]); // Add them to the back of the array
+  //     }
+  //   }
+  //   if (targetIndex < newPosition) {
+  //     for (i = 0; i < arr.length; i++) {
+  //       if ((i + steps) > arr.length - 1 ) {
+  //         // console.log(appendLast)
+  //         appendLast.push(arr[i]);
+  //       } 
+  //     }
+  //     for (i = 0; i < appendLast.length; i++) {
+  //       arr.splice((arr.length - 1), 1); // Remove the last items in the array
+  //       arr.unshift(appendLast[i]); // Add them to the front of the array
+  //     }
+  //   }
+  //   console.log(`steps: ${steps}`);
+  //   console.log(`appendLast: ${appendLast}`);
+  //   console.log(`arr after: ${arr}`);
+  // }
